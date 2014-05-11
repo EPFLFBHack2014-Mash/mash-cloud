732ad510cec9915072e7cda86d831ad059791d53
Romain Ruetschi <romain.ruetschi@gmail.com>
2014-05-11 04:23:20 +0200

    WIP

--- cloud/main.js
+++ cloud/main.js
@@ -20,6 +20,34 @@ function randomSubset(array, size)
   return shuffle(array.concat()).slice(0, size);
 }
 
+function durationSumInRange(min, max) {
+  return function(array) {
+    var duration = array.map(function(el) {
+      return +el.duration;
+    }).reduce(function(a, b) {
+      return a + b;
+    });
+
+    return min <= duration && duration <= max;
+  };
+}
+
+function findSubsetWithCriteria(array, size, p)
+{
+  var subset, tries = 0;
+
+  do {
+    subset = randomSubset(array, size);
+    tries += 1;
+  } while(!p(subset) && tries < 10);
+
+  return subset;
+}
+
+function videosAroundDuration(videos, size, sec) {
+  return findSubsetWithCriteria(videos, size, durationSumInRange(sec - 5, sec + 5));
+}
+
 function fetchVideos(group)
 {
   var query = new Parse.Query('Video');
@@ -52,7 +80,7 @@ function pushToServer(data)
 Parse.Cloud.afterSave('Video', function(req) {
   var group = req.object.get('group');
   fetchVideos(group).then(function(videos) {
-    var subset = randomSubset(videos, 6),
+    var subset = videosAroundDuration(videos, 6, 30),
         data = {
           group: group,
           videos: subset
